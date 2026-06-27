d5edee8986a8434c6023e130901903661b92be1e
chen.mugany <chen316487@gmail.com>
2026-06-27 19:08:06 +0300

    Update files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "sensor/MotionSensor.h"
-
+#include "sensor/SensorNode.hpp"
+#include "network/UdpSender.hpp"
+#include <thread>
 
 int main()
 {
     MotionSensor sensor(1);
-    sensor.runLoop();
-    
+    UdpSender sender("127.0.0.1", 9000);
+    SensorNode node(sensor, sender);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        node.tick();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
     return 0;
 }

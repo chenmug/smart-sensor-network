3988ccedc1e31a867a13ee09e3af8f4ce50db8ae
chen.mugany <chen316487@gmail.com>
2026-06-28 18:13:05 +0300

    Integrate multi-threaded sensor simulation with UDP receiver and Gateway pipeline

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include "sensor/MotionSensor.h"
 #include "sensor/SensorNode.hpp"
 #include "network/UdpSender.hpp"
+#include "network/UdpReceiver.hpp"
+#include "gateway/Gateway.hpp"
 #include <thread>
+
 
 int main()
 {
+    Gateway gateway;
+    UdpReceiver receiver(gateway, 9000);
+
     MotionSensor sensor(1);
     UdpSender sender("127.0.0.1", 9000);
     SensorNode node(sensor, sender);
 
-    for (int i = 0; i < 5; ++i)
+    std::thread receiverThread([&]()
     {
-        node.tick();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
+        receiver.run();
+    });
+
+    std::thread sensorThread([&]()
+    {
+        for (int i = 0; i < 20; ++i)
+        {
+            node.tick();
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    });
+
+    sensorThread.join();
+    receiver.stop();
+    receiverThread.join();
 
     return 0;
 }
